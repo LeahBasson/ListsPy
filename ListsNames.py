8f02f9f63b662e09a087a44bf9b0bac416f1c01b
LeahBasson <leahbasson54@gmail.com>
2025-08-15 21:23:18 +0200

    :tada:

--- ListsNames.py
+++
@@ -1,7 +0,0 @@
-# Create a list of names.
-# Use the len() function to find the number of elements in the list.
-# Print the length of the list
-
-names = ["Leah","Jamin","Zoe","Katelin"]
-
-print(len(names))
